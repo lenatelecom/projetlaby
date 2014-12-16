8219f345fca58b63358b1d70a9883d7eb61f0c8a
Lena Petersen <lena.petersen@telecom-paristech.fr>
2014-12-17 00:30:01 +0100

    Redéfinition de la classe maintest pour ouvrir une mazeWindow dont la taille est choisie par l'utilisateur dans la console

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
+
+import fr.enst.inf103.ui.MazeWindow;
+
+import maze.MazeController;
 
 public class MainTest {
 
 	
 	public static void main(String[] args) {
-	Maze maze = new Maze();
-	maze.initFromTextFile("data/labyrinthe.txt");
-	maze.saveToTextFile("data/labyrinthe2.txt");
-	}
-
+	
+	
+	 	
+	  int h = 10;
+	  int w = 10;
+	 	 try {
+	 	 	 Scanner scan = new Scanner(System.in);
+	 	 	 System.out.println("Hauteur du labyrinthe ?");
+	 	 	 h = scan.nextInt();
+	 	 	 System.out.println("Largeur du labyrinthe ?");
+	 	 	 w = scan.nextInt();
+	 	 	 scan.close();
+	 	 }
+	 	 catch (Exception e) {
+	 	 	 System.out.println("Mauvaise saisie. Le labyrinthe aura la taille par defaut.");
+	 	 }
+	 	 finally {
+	 	 	 MazeController mc = new MazeController(h,w);
+	 	 	 MazeWindow mazeWindow = new MazeWindow("My awesome laby", mc);
+	 	 }
+	    
+	 
+	  
+	     }
+	
+	   
 }
