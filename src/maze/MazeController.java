55f17a8bd05ce6161b030da8e772c2c3d3df592b
Lena Petersen <lena.petersen@telecom-paristech.fr>
2014-12-17 00:30:43 +0100

    Creation de la classe mazeController pour implémenter les methodes de l'interface du meme nom

--- src/maze/MazeController.java
+++ src/maze/MazeController.java
@@ -1,5 +1,46 @@
 package maze;
 
-public class MazeController {
 
+import fr.enst.inf103.ui.MazeViewSource;
+import fr.enst.inf103.ui.MazeViewController;
+
+
+public class MazeController implements  MazeViewController {
+
+	private MazeViewSource maze;
+	
+	public MazeController(int height, int width){
+		maze = new Maze(height,width);
+	}
+	
+	
+	@Override
+	public void calculateShortestPath() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public MazeViewSource getMazeViewSource() {
+		// TODO Auto-generated method stub
+		return maze;
+	}
+	@Override
+	public MazeViewSource newMaze() {
+		// TODO Auto-generated method stub
+		maze = new Maze(this.getHeight(),this.getWidth());
+		 return maze;
+			
+		
+	}
+	@Override
+	public MazeViewSource openMaze(String fileName) {
+		// TODO Auto-generated method stub
+		 maze.initFromTextFile(fileName);
+		return maze;
+	}
+	@Override
+	public void saveMazeAs(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
 }
