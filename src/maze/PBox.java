76da2d5f5cc0df3695ce9c88e8a6115c202f0c99
Macmac <mkr.vatinyan@gmail.com>
2015-01-05 19:56:40 +0100

    La case chemin n'est pas accéssible

--- src/maze/PBox.java
+++ src/maze/PBox.java
@@ -6,7 +6,11 @@ public class PBox extends MBox{ //Box of the Path
 
 
 
- 
+ @Override
+ public boolean isAccessible()
+	{
+		return false;
+	}
  public PBox(int line, int column){
  	 super("P",line,column);
  }
